6f46132861b7aa59a6472418050f71c79e7ddea4
Lena-user <le.lena5415@gmail.com>
2025-03-19 23:26:45 +0700

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,122 @@
-
-// Import required libraries
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 #include "DHT20.h"
+#include "DHT.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <ThingsBoard.h>
+#include <Arduino_MQTT_Client.h>
 
-// Replace with your network credentials
-// const char* ssid = PROJECT_WIFI_SSID;
-// const char* password = PROJECT_WIFI_PASSWORD;
+#define DHT_PIN 6
+#define DHT_TYPE DHT11
+DHT dht(DHT_PIN, DHT_TYPE);
+float temperature = 0;
+float humidity = 0;
 
-const char* ssid = PROJECT_WIFI_SSID;
-const char* password = PROJECT_WIFI_PASSWORD;
+WiFiClient wifiClient;
+Arduino_MQTT_Client mqttClient(wifiClient);
+ThingsBoard tb(mqttClient);
 
+// DHT20 dht;              // Create DHT20 sensor object
 
-// Set LED GPIO
-const int ledPin = 13;
-// Stores LED state
-String ledState;
+const char *ssid = "Redmi Note 11";
+const char *password = "12345671";
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+const char *token_id = "es12alt4fa25ryy4yej4";
+const char *thingBoard_Sever = "app.coreiot.io";
+const int port = 1883;
 
-// Replaces placeholder with LED state value
-String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(ledPin)){
-      ledState = "ON";
-    }
-    else{
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
-  }
-  return String();
-}
-
-// Task to handle Wi-Fi connection
-void wifiTask(void *pvParameters) {
+void wifiTask(void *pvParameters)
+{
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     Serial.println("Connecting to WiFi..");
   }
 
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
-  vTaskDelete(NULL);  // Delete the task when done
+  vTaskDelete(NULL); // Delete the task when done
 }
 
-// Task to handle server
-void serverTask(void *pvParameters) {
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    vTaskDelete(NULL);  // Delete the task if SPIFFS initialization fails
+void SensorTask(void *pvParameters)
+{
+  dht.begin();
+  while (1)
+  {
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
+    if (isnan(temperature) || isnan(humidity))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else
+    {
+      Serial.print("Temp: ");
+      Serial.print(temperature);
+      Serial.print(" *C ");
+      Serial.print("Humidity: ");
+      Serial.print(humidity);
+      Serial.println(" %");
+      tb.sendTelemetryData("temperature", temperature);
+      tb.sendTelemetryData("humidity", humidity);
+    }
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    tb.loop();
   }
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, HIGH);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-
-  // Start server
-  server.begin();
-  vTaskDelete(NULL);  // Delete the task when done
 }
 
-void setup(){
-  pinMode(ledPin, OUTPUT);
+void SeverTask(void *pvParameters)
+{
+  if (!tb.connected())
+  {
+    Serial.println("Reconnecting to ThingsBoard...");
+    while (!tb.connect(thingBoard_Sever, token_id, port))
+    {
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      Serial.println("Failed to reconnect");
+    }
+  }
+  vTaskDelete(NULL);
+}
 
-  // Create tasks for Wi-Fi and server
+void setup()
+{
+  // Initialize Serial communication to display information on Serial Monitor
+  Serial.begin(115200);
+
+  // Initialize I2C with custom SDA = 11, SCL = 12
+  // Wire.begin(11, 12);  // SDA = 11, SCL = 12
+
+  // Initialize DHT20 sensor
+  // if (!dht.begin()) {
+  //   Serial.println("Failed to initialize DHT20 sensor!");
+  //   while (1);  // Halt if sensor initialization fails
+  // }
+  // Serial.println("DHT20 sensor initialized.");
+
   xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
-  xTaskCreate(serverTask, "ServerTask", 8192, NULL, 1, NULL);
+  xTaskCreate(SensorTask, "SensorTask", 4096, NULL, 1, NULL);
+  xTaskCreate(SeverTask, "SeverTask", 4096, NULL, 1, NULL);
 }
- 
-void loop(){
-  // Nothing to do here, FreeRTOS tasks handle the work
+
+void loop()
+{
+  // if (!tb.connected())
+  // {
+  //   Serial.println("Reconnecting to ThingsBoard...");
+  //   if (!tb.connect(thingBoard_Sever, token_id, port))
+  //   {
+  //     Serial.println("Failed to reconnect");
+  //     return;
+  //   }
+  // }
+  // tb.sendTelemetryData("temperature", temperature);
+  // tb.sendTelemetryData("humidity", humidity);
+
+  // tb.loop();
 }
