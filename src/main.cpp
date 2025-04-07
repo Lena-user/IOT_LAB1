7e70d5c9d52d47e53bc0063c9912fc8df622b5ea
Lena-user <le.lena5415@gmail.com>
2025-04-07 16:10:41 +0700

    update RPC Call to control led

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,6 @@
-#include "WiFi.h"
-#include "ESPAsyncWebServer.h"
-#include "SPIFFS.h"
-#include "DHT20.h"
-#include "DHT.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include <ThingsBoard.h>
-#include <Arduino_MQTT_Client.h>
+#include "main.h"
 
-#define DHT_PIN 6
-#define DHT_TYPE DHT11
-DHT dht(DHT_PIN, DHT_TYPE);
-float temperature = 0;
-float humidity = 0;
 
-WiFiClient wifiClient;
-Arduino_MQTT_Client mqttClient(wifiClient);
-ThingsBoard tb(mqttClient);
-
-// DHT20 dht;              // Create DHT20 sensor object
-
-const char *ssid = "Redmi Note 11";
-const char *password = "12345671";
-
-const char *token_id = "es12alt4fa25ryy4yej4";
-const char *thingBoard_Sever = "app.coreiot.io";
-const int port = 1883;
 
 void wifiTask(void *pvParameters)
 {
@@ -47,8 +22,8 @@ void SensorTask(void *pvParameters)
   dht.begin();
   while (1)
   {
-    temperature = dht.readTemperature();
-    humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
 
     if (isnan(temperature) || isnan(humidity))
     {
@@ -58,14 +33,17 @@ void SensorTask(void *pvParameters)
     {
       Serial.print("Temp: ");
       Serial.print(temperature);
-      Serial.print(" *C ");
+      Serial.print(" °C ");
       Serial.print("Humidity: ");
       Serial.print(humidity);
       Serial.println(" %");
-      tb.sendTelemetryData("temperature", temperature);
-      tb.sendTelemetryData("humidity", humidity);
+      if(tb.connected())
+      {
+        tb.sendTelemetryData("temperature", temperature);
+        tb.sendTelemetryData("humidity", humidity);
+      }
     }
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
     tb.loop();
   }
 }
@@ -75,48 +53,124 @@ void SeverTask(void *pvParameters)
   if (!tb.connected())
   {
     Serial.println("Reconnecting to ThingsBoard...");
-    while (!tb.connect(thingBoard_Sever, token_id, port))
+    while (!tb.connect(thingBoard_Sever, token_id, THINGSBOARD_PORT))
     {
       vTaskDelay(1000 / portTICK_PERIOD_MS);
       Serial.println("Failed to reconnect");
     }
   }
+  if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+    const std::array<RPC_Callback, MAX_RPC_SUBSCRIPTIONS> callbacks = {
+      RPC_Callback{ RPC_SWITCH_METHOD, processSwitchChange }
+    };
+    // Perform a subscription. All consequent data processing will happen in
+    // processTemperatureChange() and processSwitchChange() functions,
+    // as denoted by callbacks array.
+    if (!rpc.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+  }
   vTaskDelete(NULL);
 }
 
+// void setup()
+// {
+//   Serial.begin(9600);
+//   while (!Serial);
+//   pinMode(LED_PIN, OUTPUT);
+//   pinMode(BUTTON_PIN, INPUT);
+
+//   xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
+//   xTaskCreate(SensorTask, "SensorTask", 4096, NULL, 1, NULL);
+//   xTaskCreate(SeverTask, "SeverTask", 4096, NULL, 1, NULL);
+// }
+
 void setup()
 {
-  // Initialize Serial communication to display information on Serial Monitor
-  Serial.begin(115200);
+    Serial.begin(9600);
+    SPI.begin();
+    rfid.PCD_Init();
 
-  // Initialize I2C with custom SDA = 11, SCL = 12
-  // Wire.begin(11, 12);  // SDA = 11, SCL = 12
+    for (byte i = 0; i < 6; i++)
+    {
+        key.keyByte[i] = 0xFF;
+    }
 
-  // Initialize DHT20 sensor
-  // if (!dht.begin()) {
-  //   Serial.println("Failed to initialize DHT20 sensor!");
-  //   while (1);  // Halt if sensor initialization fails
-  // }
-  // Serial.println("DHT20 sensor initialized.");
-
-  xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
-  xTaskCreate(SensorTask, "SensorTask", 4096, NULL, 1, NULL);
-  xTaskCreate(SeverTask, "SeverTask", 4096, NULL, 1, NULL);
+    Serial.println(F("This code scan the MIFARE Classsic NUID."));
+    Serial.print(F("Using the following key:"));
+    printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 }
 
 void loop()
 {
-  // if (!tb.connected())
-  // {
-  //   Serial.println("Reconnecting to ThingsBoard...");
-  //   if (!tb.connect(thingBoard_Sever, token_id, port))
-  //   {
-  //     Serial.println("Failed to reconnect");
-  //     return;
-  //   }
-  // }
-  // tb.sendTelemetryData("temperature", temperature);
-  // tb.sendTelemetryData("humidity", humidity);
+    if (!rfid.PICC_IsNewCardPresent())
+    {
+        return;
+    }
 
-  // tb.loop();
+    if (!rfid.PICC_ReadCardSerial())
+    {
+        return;
+    }
+
+    Serial.print(F("PICC type: "));
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    Serial.println(rfid.PICC_GetTypeName(piccType));
+
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+    {
+        Serial.println(F("Your tag is not of type MIFARE Classic."));
+        return;
+    }
+
+    if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+        rfid.uid.uidByte[1] != nuidPICC[1] ||
+        rfid.uid.uidByte[2] != nuidPICC[2] ||
+        rfid.uid.uidByte[3] != nuidPICC[3])
+    {
+        Serial.println(F("A new card has been detected."));
+
+        for (byte i = 0; i < 4; i++)
+        {
+            nuidPICC[i] = rfid.uid.uidByte[i];
+        }
+
+        Serial.println(F("The NUID tag is:"));
+        Serial.print(F("In hex: "));
+        printHex(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println();
+        Serial.print(F("In dec: "));
+        printDec(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println();
+    }
+    else
+        Serial.println(F("Card read previously."));
+
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+}
+
+void printHex(byte *buffer, byte bufferSize)
+{
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
+
+void printDec(byte *buffer, byte bufferSize)
+{
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], DEC);
+    }
 }
